fd6106fe825cbe82faa9bdc57fd889adce8502f2
simoxxhunter <kingstonegame@gmail.com>
2024-08-19 22:24:37 +0100

    initialisation du projet

---
+++ src/main/java/com/Colocation/ColocationApplication.java
@@ -0,0 +1,13 @@
+package com.Colocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Colocation/ColocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Colocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
