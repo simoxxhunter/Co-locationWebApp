e5affcd84c8d431e6c8364f590afc16efdd303eb
simoxxhunter <kingstonegame@gmail.com>
2024-09-02 13:52:23 +0100

    model listing

---
+++ src/main/java/com/colocation/models/ListingsModel.java
@@ -0,0 +1,41 @@
+package com.colocation.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ListingsModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private double rent;
+    private int bedrooms;
+    private int bathrooms;
+    private String address;
+
+    @ElementCollection
+    private List<String> amenities;
+
+    @ElementCollection
+    private List<String> photos;
+
+    private boolean available;
+
+    // Custom methods can be added here if needed
+    public void markAsUnavailable() {
+        this.available = false;
+    }
+}
