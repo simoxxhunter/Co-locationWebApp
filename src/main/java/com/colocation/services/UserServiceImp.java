9cf83be96b3b208a8ea2b674af6f655f9ba04cd2
simoxxhunter <kingstonegame@gmail.com>
2024-09-03 22:31:08 +0100

    implimentation de service user

---
+++ src/main/java/com/colocation/services/UserServiceImp.java
@@ -0,0 +1,27 @@
+package com.colocation.services;
+
+import com.colocation.models.ProfileModel;
+import com.colocation.models.UserModel;
+import com.colocation.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImp {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    public ProfileModel getProfile(Long userId) {
+        UserModel user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (user.getProfiles() != null && !user.getProfiles().isEmpty()) {
+            return user.getProfiles().iterator().next();
+        } else {
+            throw new RuntimeException("Profile not found");
+        }
+    }
+}
