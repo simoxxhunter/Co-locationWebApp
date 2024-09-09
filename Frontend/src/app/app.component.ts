86ceab9afcad784defe1794de323dc436343c7bc
simoxxhunter <kingstonegame@gmail.com>
2024-09-09 20:38:00 +0100

    telechargment de angular material

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+logout() {
+throw new Error('Method not implemented.');
+}
   title = 'ColocationAngular';
 }
