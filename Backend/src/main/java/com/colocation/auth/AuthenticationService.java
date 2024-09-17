c0ba7773afeb9a7e625b3f1e8a0538200df5debb
simoxxhunter <kingstonegame@gmail.com>
2024-09-17 12:17:18 +0100

    logic pour faire le login et enregistrement

---
+++ Backend/src/main/java/com/colocation/auth/AuthenticationService.java
@@ -0,0 +1,60 @@
+package com.colocation.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.colocation.services.*;
+import com.colocation.repositories.*;
+import com.colocation.models.*;
+import com.colocation.config.*;
+import com.colocation.auth.*;
+
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepo repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = UserModel.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .role(user.getRole().name())
+                .build();
+    }
+
+}
