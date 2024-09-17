c290f3104338837f7ff9976de21a624af2f947ac
simoxxhunter <kingstonegame@gmail.com>
2024-09-17 12:14:49 +0100

    logic de token JWT

--- Backend/src/main/java/com/colocation/repositories/UserRepo.java
+++ Backend/src/main/java/com/colocation/repositories/UserRepo.java
@@ -3,5 +3,8 @@ package com.colocation.repositories;
 import com.colocation.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByEmail(String email);
 }
