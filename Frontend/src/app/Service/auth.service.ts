2dce301b452899bad22ef884c94517fbc6e78a8c
simoxxhunter <kingstonegame@gmail.com>
2024-09-22 17:50:26 +0100

    authentification service front

---
+++ Frontend/src/app/Service/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8083/api/v1/auth';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/authenticate`, { email, password });
+  }
+
+  register(email: string, password: string, role: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, { email, password, role });
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  getRole(): string | null {
+    return localStorage.getItem('role');
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('role');
+    this.router.navigate(['/login']);
+  }
+}
