a745cb63605fe4a663b2e73c1689e6ca08f80456
simoxxhunter <kingstonegame@gmail.com>
2024-09-22 17:28:56 +0100

    creation de chemin pour la page create listing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './Components/index/index.component';
+import { LoginComponent} from "./Components/login/login.component";
+import {ListingsComponent} from "./Components/listings/listings.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "index", component : IndexComponent},
+  {path : "login", component : LoginComponent },
+  {path : "createListing", component : ListingsComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
