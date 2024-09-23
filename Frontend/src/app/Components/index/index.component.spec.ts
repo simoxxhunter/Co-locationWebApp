1ea0dad8f94ff6ad6010e4896e81c03126037976
simoxxhunter <kingstonegame@gmail.com>
2024-09-23 20:23:06 +0100

    modufication sur angular styles module

--- Frontend/src/app/Components/index/index.component.spec.ts
+++ Frontend/src/app/Components/index/index.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { IndexComponent } from './index.component';
+import { IndexhomeComponent } from './index.component';
 
-describe('IndexComponent', () => {
-  let component: IndexComponent;
-  let fixture: ComponentFixture<IndexComponent>;
+describe('IndexhomeComponent', () => {
+  let component: IndexhomeComponent;
+  let fixture: ComponentFixture<IndexhomeComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [IndexComponent]
+      declarations: [IndexhomeComponent]
     });
-    fixture = TestBed.createComponent(IndexComponent);
+    fixture = TestBed.createComponent(IndexhomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
