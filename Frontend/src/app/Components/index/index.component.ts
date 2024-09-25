d6653c9c66bac9d6b00b9ba9abc24ea99dfae134
simoxxhunter <kingstonegame@gmail.com>
2024-09-25 19:07:28 +0100

    jai trouver un solution pour l'erreur d'affichage

--- Frontend/src/app/Components/index/index.component.ts
+++ Frontend/src/app/Components/index/index.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
-})
-export class IndexComponent {
+  styleUrls: ['./index.component.scss', '../../../assets/css/normalize.scss','../../../assets/css/webflow.scss' , '../../../assets/css/simos-stupendous-site-472b4d.webflow.scss'],
 
+})
+export class IndexhomeComponent {
+  ngOnInit() {
+    const script = document.createElement('script');
+    script.src = 'assets/js/webflow.js';
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
+  }
+  
 }
