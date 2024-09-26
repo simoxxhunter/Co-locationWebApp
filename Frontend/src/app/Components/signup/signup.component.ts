194f40ca4036dc3c0aade0d1efcb488e071d3a62
simoxxhunter <kingstonegame@gmail.com>
2024-09-26 23:15:42 +0100

    method register angular

--- Frontend/src/app/Components/signup/signup.component.ts
+++ Frontend/src/app/Components/signup/signup.component.ts
@@ -1,17 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Service/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+
+
+export class SignupComponent implements OnInit{
+
+  signupForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+
   ngOnInit() {
     const script = document.createElement('script');
     script.src = 'assets/js/webflow.js';
     script.type = 'text/javascript';
     document.body.appendChild(script);
+
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]})
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      const formData = {
+        email: this.signupForm.value.email,
+        password: this.signupForm.value.password,
+        firstName: this.signupForm.value.firstName,
+        lastName: this.signupForm.value.lastName,
+        username: this.signupForm.value.username,
+        phoneNumber: this.signupForm.value.phoneNumber,
+        role : 'USER'
+
+      };
+  
+      this.authService.register(formData).subscribe(
+        response => {
+          this.authService.saveToken(response.token);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.error('Registration failed', error);
+        }
+      );
+    }
   }
   
+
 
 }
