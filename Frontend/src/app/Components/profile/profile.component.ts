a8ffd9867223b1c131e0c80994a3ac49298b52b7
simoxxhunter <kingstonegame@gmail.com>
2024-09-27 20:36:42 +0100

    importation de script JS

--- Frontend/src/app/Components/profile/profile.component.ts
+++ Frontend/src/app/Components/profile/profile.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
+  ngOnInit() {
+    const script = document.createElement('script');
+    script.src = 'assets/js/webflow.js';
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
+    
+  }
 }
