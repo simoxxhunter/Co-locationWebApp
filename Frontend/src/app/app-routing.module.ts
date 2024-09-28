04fd616532594e56d39b5f75ef08b4aba3c9acc8
simoxxhunter <kingstonegame@gmail.com>
2024-09-28 18:02:04 +0100

    ajoute de contact us path

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent} from "./Components/login/login.component";
 import {ListingsComponent} from "./Components/listings/listings.component";
 import { SignupComponent } from './Components/signup/signup.component';
 import { ProfileComponent } from './Components/profile/profile.component';
+import { ContactComponent } from './Components/contact/contact.component';
 const routes: Routes = [
   {path : "index", component : IndexhomeComponent},
   {path : "login", component : LoginComponent },
   {path : "createListing", component : ListingsComponent },
   {path : "register" , component : SignupComponent},
-  {path : "user-profile" , component : ProfileComponent}
+  {path : "user-profile" , component : ProfileComponent},
+  {path : "contact-us" , component : ContactComponent}
 
 ];
 
