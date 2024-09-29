268c113f4d0a2b381b85bb83ada583053e8649dc
simoxxhunter <kingstonegame@gmail.com>
2024-09-29 21:20:17 +0100

    option pour faire le logout et supprimer le JWT TOKEN

--- Frontend/src/app/Components/navbar/navbar.component.ts
+++ Frontend/src/app/Components/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/Service/Authentification/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
-  logout() {
+  constructor(private AuthService: AuthService, private router: Router) {}
+  ngOnInit(): void {
     throw new Error('Method not implemented.');
+  }
+
+  logout(): void {
+    this.AuthService.logout();
+    this.router.navigate(['/login']);
   }
   title = 'ColocationAngular';
 }
