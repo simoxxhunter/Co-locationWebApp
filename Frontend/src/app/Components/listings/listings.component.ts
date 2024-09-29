8cdb36751e1d8cf553be49050d8dd547c86acac6
simoxxhunter <kingstonegame@gmail.com>
2024-09-29 21:19:19 +0100

    logic pour prendre les annonces depuis la DB

--- Frontend/src/app/Components/listings/listings.component.ts
+++ Frontend/src/app/Components/listings/listings.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ListingsModel } from 'src/app/models/ListingsModel';
+import { ListingService } from 'src/app/Service/Listing/listing.service';
 
 @Component({
   selector: 'app-listings',
   templateUrl: './listings.component.html',
   styleUrls: ['./listings.component.scss']
 })
-export class ListingsComponent {
+export class ListingsComponent implements OnInit{
+
+  listings: ListingsModel[] = [];
+
+  constructor(
+    private listingService: ListingService
+  ) {
+  }
+
+
+  ngOnInit() {
+    const script = document.createElement('script');
+    script.src = 'assets/js/webflow.js';
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
+    this.loadListings();
+    
+  }
+
+
+  loadListings(): void {
+    this.listingService.getListings().subscribe(data => {
+      this.listings = data;
+    });}
+
+
 
 }
