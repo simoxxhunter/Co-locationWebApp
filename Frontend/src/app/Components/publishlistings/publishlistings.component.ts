cfe8db1e81b57513ce220f93c1683662fade69c2
simoxxhunter <kingstonegame@gmail.com>
2024-09-29 21:21:23 +0100

    component pour publier une annonce avec fomulaire

---
+++ Frontend/src/app/Components/publishlistings/publishlistings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publishlistings',
+  templateUrl: './publishlistings.component.html',
+  styleUrls: ['./publishlistings.component.scss']
+})
+export class PublishlistingsComponent {
+
+}
